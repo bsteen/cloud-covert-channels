cedb06c58c5897eb5c2b7be26898595b5596a90a
bsteen <steenkamerben@gmail.com>
2018-01-14 17:38:44 -0500

    Added channel calibration functions

--- proc-meminfo/meminfo.cpp
+++ proc-meminfo/meminfo.cpp
@@ -1,22 +1,31 @@
 // meminfo.cpp
-// Interface functions used by source.c and sink.c to read from /proc/meminfo.c
+// Interface functions used by source.cpp and sink.cpp to read from /proc/meminfo
 
 #include "meminfo.hpp"
 #include <fstream>
 #include <iostream>
 #include <stdlib.h>
-#include <string>
 #include <time.h>
 #include <unistd.h>
-using namespace std;
+#include <vector>
 
-unsigned long mem_free = 0;     // Amount of RAM in kB left unused by the system
+unsigned long mem_free = 0;         // Amount of RAM in kB left unused by the system
+unsigned long base_mem_free = 0;    // The average (baseline) MemFree value calculated before the channel begins transmitting; Used as the "null" value during transmission
+                                    // with null being neither 0 or 1
+
+vector<unsigned long> trans_readings;       // Transmission readings; Values read from MemFree line of /proc/meminfo during channel transmission
+vector<unsigned long> calib_readings;       // Calibration readings; Recorded values of MemFree before transmission starts; used for calculating base_mem_free
 
 unsigned long get_mem_free(){
     return mem_free;
 }
 
-// Returns the kB value of line; e.g.: "MemFree:        11528160 kB" => "11528160"
+unsigned long get_base_mem_free(){
+    return base_mem_free;
+}
+
+// Returns the kB value the MemFree line from /proc/meminfo;
+// e.g.: takes in "MemFree:        11528160 kB" and returns "11528160"
 string extract_mem_val(string line){
     int start = 0;
     int end = 0;
@@ -40,11 +49,10 @@ string extract_mem_val(string line){
 
     // Return the number only
     return line.substr(start, end - start);
-
 }
 
 // Get the current MemFree value from /proc/meminfo and store it in mem_free
-// Takes, on average 0.0001 sec (100 us) to run
+// Average execution time: 0.0001 sec (100 us)
 void update_mem_free(){
     ifstream file;
     file.open("/proc/meminfo");
@@ -63,6 +71,52 @@ void update_mem_free(){
     }
 
     return;
+}
+
+// Gets the newest value of mem_free and adds it to the vector of calib_readings
+// Average execution time: 
+void record_calib_reading(){
+    update_mem_free();
+    calib_readings.push_back(mem_free);
+    return;
+}
+
+// Gets the newest value of mem_free and adds it to the vector of trans_readings
+// Average execution time: 
+void record_trans_reading(){
+    update_mem_free();
+    trans_readings.push_back(mem_free);     // POTENTIAL ISSUE: As the program runs more memory is needed to store the recordings, which will affect the value of MemFree
+                                            // Fix: Instead of storing in vector, write value to file; This would be much slower, however
+    return;
+}
+
+// Calculate the average (baseline) value of MemFree by find the average of the values stored in calibration
+void calc_base_mem_free(){
+    unsigned long sum = 0;
+
+    for(int i = 0; i < calib_readings.size(); i++){
+        sum += calib_readings[i];
+    }
+
+    base_mem_free = sum / calib_readings.size();
+    return;
+}
+
+// Calculates the the baseline value of MemFree and returns the value
+// Called by source and sink before the channel starts to get the "null" transmission value
+unsigned long do_channel_calibartion(){
+    struct timespec start, current;
+    unsigned long elapsed_nano_sec = 0;
+    unsigned long calib_time= CALIB_TIME * 1000000000;
+
+    while(elapsed_nano_sec < calib_time){
+        record_calib_reading();     // Take another calibation reading
+        usleep(CALIB_DELAY);        // Wait this amount of microseconds before the next reading
+    }
+
+    // Calculate the baseline MemFree value and then return it
+    calc_base_mem_free();
+    return base_mem_free;
 }
 
 // For testing functions; not used in actual application
--- proc-meminfo/meminfo.hpp
+++ proc-meminfo/meminfo.hpp
@@ -1,9 +1,20 @@
 // meminfo.hpp
 // Defines interface functions in meminfo.c used by source.c and sink.c
 // Defines costants used by meminfo.c, source.c, and sink.c
+#include <string>
+using namespace std;
 
-#define HIGH_BIT_ALLOC 200000       // Number of kB above baseline memory usage for sink to detect a '1'
-#define LOW_BIT_ALLOC 100000        // Number of kB above baseline memory usage for sink to detect a '0'
-#define BASELINE_AVG_TIME 10        // Amount of time source/sink will spend calculating the average 
-                                    // baseline memory usage before starting the channel; NEED TO IMPLEMENT THIS
+#define HIGH_BIT_ALLOC 200000   // Number of kB above baseline memory usage for sink to detect a '1'
+#define LOW_BIT_ALLOC 100000    // Number of kB above baseline memory usage for sink to detect a '0'
+#define CALIB_TIME 5            // Amount of seconds source/sink will spend calculating the average baseline memory usage before starting the channel
+#define CALIB_DELAY 100000      // Number of microseconds between each calibration recording; Smaller number means more calibration recordings will be made
+#define NUM_CONSEC_VAL 2        // Number of consecutive reading of the same type (1, 0, or null) that need to occur for a value to be recognized.
 
+unsigned long get_mem_free();           // Used by source and sink
+unsigned long get_base_mem_free();      // Used by source and sink
+string extract_mem_val(string line);    // Used internally meminfo.cpp
+void update_mem_free();                 // Used internally meminfo.cpp
+void record_calib_reading();            // Used internally meminfo.cpp
+void record_trans_reading();            // Used by sink
+void calc_base_mem_free();              // Used internally meminfo.cpp
+unsigned long do_channel_calibartion(); // Used by source and sink
